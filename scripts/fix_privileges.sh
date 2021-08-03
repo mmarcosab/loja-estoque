13faaaa6e589348d52118f3fb0aad7e56baf5d68
José Wesley Silva de Oliveira <josewso55@gmail.com>
2021-08-03 12:58:03 -0300

    corrige acessos de arquivos

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+chmod +x /home/ec2-user/*.jar
+chmod +x /home/ec2-user/server_start.sh
+chmod +x /home/ec2-user/server_stop.sh
